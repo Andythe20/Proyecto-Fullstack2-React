c02ed7052e3b34a5e189d0da2722272ca7ac7d30
Andrés Ortega <andythe20xd@gmail.com>
2025-10-08 12:19:30 -0300

    fix/ pagina contacto arreglada, se arreglaron varios archivos qque fueron modificados

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,22 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 
-import Contacto from "../pages/Contacto";
+import Contacto from "../pages/Contacto/Contacto";
 import Inicio from "../pages/Inicio";
 import Nosotros from "../pages/Nosotros";
 import Productos from "../pages/Productos";
 
-function AppRoutes(){
-    return(
-        <>
-            <Routes>
-                <Route path="/" element={<Inicio/>} />
-                <Route path="/nosotros" element={<Nosotros/>} />
-                <Route path="/productos" element={<Productos/>} />
-                <Route path="/contacto" element={<Contacto/>} />
-            </Routes>
-        </>
-    )
+function AppRoutes() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+    </>
+  );
 }
 
-
-export default AppRoutes
+export default AppRoutes;
