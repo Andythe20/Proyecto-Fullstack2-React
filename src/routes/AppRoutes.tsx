0ea51ca9b4db2c6ebbafa3b6eefc432566b870cb
L-Maulen <l.maulen@duocuc.cl>
2025-10-08 22:38:06 -0300

    Add sobre nosotros texto y cartas

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Contacto from "../pages/Contacto/Contacto";
 import Inicio from "../pages/Inicio";
-import Nosotros from "../pages/Nosotros";
+import Nosotros from "../pages/Sobre-nosotros/Nosotros.tsx";
 import Productos from "../pages/Productos";
 
 function AppRoutes() {
