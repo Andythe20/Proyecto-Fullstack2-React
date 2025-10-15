01913102f6e3a140017f3856ec6fb07d184c77eb
Andrés Ortega <andythe20xd@gmail.com>
2025-10-14 23:27:19 -0300

    fix/ se arreglo el componente Field y la pagina Contacto. Tambien se modifico el componente Button, pero sin afectar su funcionalidad

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -4,49 +4,57 @@ interface FieldProps {
   txt: string;
   required?: boolean;
   id: string;
-  htmlFor: string;
   type?: string;
   as?: "input" | "textarea";
-  onBlurFunction?: () => void;
+  value?: string;
+  onChange?: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  error?: string;
 }
 
 function Field({
   className,
-  htmlFor,
   title,
   id,
   type = "text",
   txt,
   required,
   as = "input",
-  onBlurFunction,
+  value,
+  onChange,
+  error,
 }: FieldProps) {
   return (
     <div className={className}>
-      <label htmlFor={htmlFor} className="form-label">
+      <label htmlFor={id} className="form-label">
         {title}
       </label>
       {as === "textarea" ? (
         <textarea
           id={id}
           name={id}
-          className="form-control"
+          className={`form-control ${error ? "is-invalid" : ""}`}
           placeholder={txt}
           required={required}
-          onBlur={onBlurFunction}
           rows={5}
+          value={value}
+          onChange={onChange}
         />
       ) : (
         <input
           id={id}
           type={type}
-          className="form-control"
+          className={`form-control${error ? " is-invalid" : ""}`}
           placeholder={txt}
           required={required}
-          onBlur={onBlurFunction}
+          value={value}
+          onChange={onChange}
         />
       )}
-      <div className="invalid-feedback">El {htmlFor} es inválido</div>
+      <div className="invalid-feedback">
+        {error ? error : `El ${id} es inválido`}
+      </div>
     </div>
   );
 }
