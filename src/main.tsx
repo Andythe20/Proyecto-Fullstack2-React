3675fafe27001c8017a3cce4a7ae4e559e67d998
Vina del Mar <vina@duoc.acad>
2025-10-15 09:52:10 -0300

    Update se implementa boton hamburguesa y seccion del inicio

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 
 import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Navbar from './components/Navbar/Navbar.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes.tsx'
